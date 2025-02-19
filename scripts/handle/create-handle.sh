8b0a7eb67a39e50eef2acafa4bdee4e2c76b0bd0
Stefan Probst <stefanprobst@posteo.de>
2025-02-19 18:05:24 +0100

    chore: doi

--- scripts/handle/create-handle.sh
+++ scripts/handle/create-handle.sh
@@ -3,13 +3,13 @@
 files=$(git diff --diff-filter=AMR --name-only ${VERCEL_GIT_PREVIOUS_SHA} ${VERCEL_GIT_COMMIT_SHA} -- content/**/*.mdx | xargs)
 
 for file in $files; do
-	node --experimental-strip-types ./scripts/doi/create-doi.ts --resouce $file
+	pnpm tsx ./scripts/handle/create-handle.ts --resource $file
 	git add $file
 done
 
 if [[ -n "$(git diff --staged)" ]]; then
 	git config user.name "${GITHUB_USERNAME}"
 	git config user.email "${GITHUB_EMAIL}"
-	git commit -m "content: add doi [skip ci]"
+	git commit -m "content: add handle [skip ci]"
 	git push origin
 fi
